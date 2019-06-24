155a4b92884ece7d52ab165a9325819f40240a8d
Bartjan Hoogenbosch <bartjanh@uxx-001.pc-mania.nl>
2019-06-24 16:25:39 +0200

    Made it possible to run it with output on screen. When added with the -m of --mail option, mail is send

--- cc-stats.py
+++ cc-stats.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3.5
+#!/usr/local/bin/python3.7
 #
 # Created 9-Feb 2019
 # @author: bartjan@pc-mania.nl
 # https://github.com/barreljan/cc-stats
-# 
+#
 import requests
 import sys
 import smtplib
@@ -11,20 +11,36 @@ import collections
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+# Assets dict: TOKEN as Key, int as Value
+# TOKEN must be the official token code used on the CMC website
 assets = {
     'XRP': 20.57,
-    'STRAT': 29.99,
+    'STRAT': 29.99
           }
 
+to_mail = False
 smtpserver = 'smtp.somehost.com'
 smtpport = 25
 sender = 'noreply@yourdomain.org'
 rcpt = 'john@doe.net'
+server = smtplib.SMTP(smtpserver, smtpport)
 
 
-def cryptodata(assets):
+def argparse():
+    global to_mail
+
+    if len(sys.argv) >= 2:
+        if sys.argv[1] == '-h' or sys.argv[1] == '--help':
+            print("Syntax is {0} -m or --mail\nDefault output is to screen\n".format(sys.argv[0]))
+            sys.exit()
+        elif sys.argv[1] == '-m' or sys.argv[1] == '--mail':
+            to_mail = True
+
+
+def cryptodata(_assets):
+    # Generate a string of coins pieced together for the API call
     coins = str()
-    for coin, qty in assets.items():
+    for coin, qty in _assets.items():
         if coins is "":
             coins += coin
         else:
@@ -37,15 +53,16 @@ def cryptodata(assets):
         'Accept': 'application/json'
     }
 
+    # Do the call
     try:
         data = requests.get(link, headers=headers)
     except Exception:
         print("Could not connect")
         sys.exit(1)
 
-    jdata = data.json()
-
+    # Return the data
     try:
+        jdata = data.json()
         if jdata['data']:
             return jdata['data']
         elif not jdata['data']:
@@ -55,34 +72,51 @@ def cryptodata(assets):
         sys.exit(1)
 
 
-# Run it
-allcrypto = cryptodata(assets)
-allcrypto = collections.OrderedDict(sorted(allcrypto.items()))
+def process_data():
+    global to_mail
 
-msg = MIMEMultipart('alternative')
-msg['Subject'] = 'Latest coin prices'
-msg['From'] = sender
-msg['To'] = rcpt
+    # Retrieve coin info for the available assets
+    allcrypto = cryptodata(assets)
+    allcrypto = collections.OrderedDict(sorted(allcrypto.items()))
 
-msgbody = """\
-<html>
-  <head></head>
-  <body style="font-family: arial;font-style:normal;font-size:12px">
-    <p>Crypto Summary</p>
-    <table style="font-family: arial;font-style:normal;font-size:12px;text-align:left">
-      <tr><th style="text-align:left">Coin</th><th style="text-align:left">Qty</th><th style="text-align:left">Price</th><th style="text-align:left">Totals (USD)</th></tr>
-"""
+    # Generate the output
+    emailmsg = MIMEMultipart('alternative')
+    emailmsg['Subject'] = 'Latest coin prices'
+    emailmsg['From'] = sender
+    emailmsg['To'] = rcpt
+    emailmsgbody = "<html>\n\t<head></head>\n\t<body style=\"font-family: arial;font-style:normal;font-size:12px\">\n"
+    emailmsgbody += "\t\t<p>Crypto Summary</p>\n\t\t<table style=\"font-family: arial;font-style:normal;font-size:12px;"
+    emailmsgbody += "text-align:left\">\n\t\t\t<tr><th style=\"text-align:left\">Coin</th><th "
+    emailmsgbody += "text-align:left\">Qty</th><th style=\"text-align:left\">Price</th><th style=\"text-align:left\">"
+    emailmsgbody += "(USD)</th></tr>\n"
+    screenmsg = "{}\n\n".format(emailmsg['Subject'])
 
-totalval = 0
-for coin, coinitems in allcrypto.items():
-    price = coinitems['quote']['USD']['price']
-    assetval = price*assets[coin]
-    totalval += assetval
-    msgbody += "      <tr><td>{0}</td><td>{1}</td><td>{2:.2f}</td><td>{3:.2f}</td></tr>\n".format(coin, assets[coin], price, assetval)
+    totalval = 0
+    for coin, coinitems in allcrypto.items():
+        price = coinitems['quote']['USD']['price']
+        assetval = price*assets[coin]
+        totalval += assetval
+        emailmsgbody += "\t\t\t<tr><td>{0}</td><td>{1}</td><td>{2:.2f}</td><td>{3:.2f}</td></tr>\n"\
+            .format(coin, assets[coin], price, assetval)
+        screenmsg += "{0}\t\t{1}\t\t{2:.2f}\t\t{3:.2f}\n".format(coin, assets[coin], price, assetval)
 
-msgbody += "    </table>\n    <br>\n    <p>Total value: {}<p>\n  </body>\n</html>\n".format(round(totalval, 2))
-msg.attach(MIMEText(msgbody, 'html'))
+    emailmsgbody += "\t\t</table>\n\t\t<br>\n\t\t<p>Total value: {}<p>\n\t</body>\n</html>\n".format(round(totalval, 2))
+    emailmsg.attach(MIMEText(emailmsgbody, 'html'))
+    screenmsg += "\nTotal Value: {}".format(round(totalval, 2))
 
-server = smtplib.SMTP(smtpserver, smtpport)
-server.sendmail(sender, rcpt, msg.as_string())
+    # Return output
+    if to_mail:
+        return emailmsg
+    else:
+        return screenmsg
+
+
+if __name__ == '__main__':
+    argparse()
+    output = process_data()
+
+    if to_mail:
+        server.sendmail(sender, rcpt, output.as_string())
+    else:
+        print(output)
 
