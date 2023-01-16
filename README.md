01f3264258c0fb070b1bf7e326739937a28674ba
barreljan <bartjan@pc-mania.nl>
2023-01-16 20:50:03 +0100

    Moved the user variables to YAML file and restructured the code

--- README.md
+++ README.md
@@ -5,26 +5,46 @@ A simple api call tool for getting a small amount of coin stats
 to send to your email box on a daily (or whatever you want) basis.
 
 ## Requirements
-- Python 3.x (Developped in 3.7, tested in 3.5)
-- Modules requests, sys, smtplib
-- A valid Pro account with a key from Coinmarketcap
+- Python 3.6 or higher
+- A valid Pro account with an API key from coinmarketcap.com
 - Crypto currency
 
 ## API Key
 You can register yourself to https://pro.coinmarketcap.com for
 a free key. You can spend up to 10k a month in requests.
-Use the key in the 'api_key' variable.
 
-## Currency
-Based on your assets, you can add keys and values to the 'assets'
- dict. The key must be matching to the actual coin code used at
-Coinmarketcap. The value is your actual number of coins.
+## How to use
+
+Copy the `cc-stat.yml_example` to `cc-stats.yml`.
+
+In this `cc-stats.yml` file you can add your assets, as a simple list.
+The assets needs to valid with coinmarketcap.com and need to be in
+capital.
+
+For the email part, of course, you need to correct the settings to
+your need and situation. 
+
+After that you can run it:
+
+```
+python3 cc-stats.py
+```
+
+Or to email:
+
+```
+python3 cc-stats.py -m
+```
 
 ## Output
 ```
-Coin    Qty     Price       Totals (USD)
-BTC     2       3651.62:    7303.24
-ETH     1       117.69 :    117.69
+Latest coin prices
+
+Coin        Qty     Price       Total (USD)
+ETH         1       1586.11     1586.11
+USDT        20.5    1.00        20.50
+
+Total Value: 1606.61
 ```
 
 Enjoy!
--- cc-stats.py
+++ cc-stats.py
@@ -1,57 +1,59 @@
-#!/usr/local/bin/python3.7
-#
-# Created 9-Feb 2019
-# Update 16-11-2020
-# @author: bartjan@pc-mania.nl
-# https://github.com/barreljan/cc-stats
-#
+#!/bin/env python3
+"""
+ @author: bartjan@pc-mania.nl
+ https://github.com/barreljan/cc-stats
+"""
 import requests
 import sys
+import argparse
+import os
 import smtplib
 import collections
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from yaml import load, dump
+from yaml import Loader, Dumper
 
-# Assets dict: TOKEN as Key, int as Value
-# TOKEN must be the official token code used on the CMC website
-assets = {
-    'XRP': 21.57,
-    'STRAT': 29.99
-}
+path = os.path.dirname(__file__)
 
-to_mail = False
-
-SMTP_SERVER = "smtp.somehost.com"
-SMTP_PORT = 25
-SENDER = "noreply@yourdomain.org"
-RCPT = "john@doe.net"
-API_KEY = "your Coinmarketcap Pro key"
-
-
-def argparse():
-    global to_mail
-
-    if len(sys.argv) >= 2:
-        if sys.argv[1] == '-h' or sys.argv[1] == '--help':
-            print(f"Syntax is {sys.argv[0]} -m or --mail\nDefault output is to screen\n")
-            sys.exit()
-        elif sys.argv[1] == '-m' or sys.argv[1] == '--mail':
-            to_mail = True
+try:
+    if os.path.exists(f'{path}/cc-stats.yml'):
+        stream = open(f'{path}/cc-stats.yml', 'r')
+        config = load(stream, Loader=Loader)
+        assets = config['assets']
+    else:
+        raise SystemExit('No \'cc-stats.yml\' found, exiting...')
+    
+    smtp_server = config['email_settings']['smtp_server']
+    smtp_port = config['email_settings']['smtp_port']
+    sender = config['email_settings']['sender']
+    rcpt = config['email_settings']['rcpt']
+    api_key = config['cmc']['api_key']
+except KeyError:
+    raise SystemExit('There is something wrong with the config\nSee the \'cc-stats.yml_example\'')
 
 
-def get_crypto_data(_assets):
+def parse_args() -> object:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--mail', dest='to_mail', action='store_true',
+                        help='Sends the result in a (HTML) email')
+
+    return parser.parse_args()
+
+
+def get_crypto_data(_assets: dict) -> dict:
     """
     Function to make the request to the remote server with given assets.
 
     :param _assets: dict containing assets
-    :return: json
+    :return: dict 
     """
     # Generate a string of coins pieced together for the API call
-    coins = ",".join(assets)
+    coins = ','.join(assets)
 
-    link = f"https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol={coins}"
+    link = f'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol={coins}'
     headers = {
-        'X-CMC_PRO_API_KEY': API_KEY,
+        'X-CMC_PRO_API_KEY': api_key,
         'Accept': 'application/json'
     }
 
@@ -59,7 +61,7 @@ def get_crypto_data(_assets):
     try:
         data = requests.get(link, headers=headers)
     except (requests.ConnectionError, requests.ConnectTimeout, requests.TooManyRedirects, requests.HTTPError):
-        raise SystemExit("Could not connect")
+        raise SystemExit('Could not connect')
 
     # Return the data
     try:
@@ -69,34 +71,33 @@ def get_crypto_data(_assets):
         else:
             return jdata['data']
     except KeyError as e:
-        raise SystemExit(f"API Call went wrong, no usable data returned: {e}")
+        raise SystemExit(f'API Call went wrong, no usable data returned: {e}')
 
 
-def process_data():
+def report_data(data) -> tuple[str, str]:
     """
     Function to present the data in a formatted manner, either on screen or via (html) email.
-    """
-    global to_mail
 
-    # Retrieve coin info for the available assets
-    all_crypto = get_crypto_data(assets)
-    all_crypto = collections.OrderedDict(sorted(all_crypto.items()))
+    :param data: dict
+    :returns tuple: ('string with email message', 'string with screen message')
+    """
 
     # Generate the output
     email_msg = MIMEMultipart('alternative')
     email_msg['Subject'] = "Latest coin prices"
-    email_msg['From'] = SENDER
-    email_msg['To'] = RCPT
+    email_msg['From'] = sender
+    email_msg['To'] = rcpt
     email_msg_body = """
         <html>\n\t<head></head>\n\t<body style=\"font-family: arial;font-style:normal;font-size:12px\">\n
         \t\t<p>Crypto Summary</p>\n\t\t<table style=\"font-family: arial;font-style:normal;font-size:12px;
         text-align:left\">\n\t\t\t<tr><th style=\"text-align:left\">Coin</th><th 
         text-align:left\">Qty</th><th style=\"text-align:left\">Price</th><th style=\"text-align:left\">
         Totals (USD)</th></tr>\n"""
-    screen_msg = f"{email_msg['Subject']}\n\nCoin\t\tQty\t\tPrice\t\tTotal (USD)\n"
+    email_subject = email_msg['Subject']
+    screen_msg = f'{email_subject}\n\nCoin\t\tQty\t\tPrice\t\tTotal (USD)\n'
 
     total_value = 0
-    for coin, coin_items in all_crypto.items():
+    for coin, coin_items in data.items():
         price = coin_items['quote']['USD']['price']
         asset_value = price * assets[coin]
         total_value += asset_value
@@ -106,22 +107,26 @@ def process_data():
 
     email_msg_body += f"\t\t</table>\n\t\t<br>\n\t\t<p>Total value: {round(total_value, 2)}<p>\n\t</body>\n</html>\n"
     email_msg.attach(MIMEText(email_msg_body, 'html'))
-    screen_msg += f"\nTotal Value: {round(total_value, 2)}"
+    screen_msg += f'\nTotal Value: {round(total_value, 2)}'
 
     # Return output
-    if to_mail:
-        return email_msg
-    else:
-        return screen_msg
+    return email_msg, screen_msg
 
 
 if __name__ == '__main__':
-    argparse()
-    output = process_data()
+    options = parse_args()
 
-    if to_mail:
-        server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
-        server.sendmail(SENDER, RCPT, output.as_string())
+    # Retrieve coin info for the available assets and sort them
+    all_crypto = get_crypto_data(assets)
+    all_crypto = collections.OrderedDict(sorted(all_crypto.items()))
+
+    # Get the report(s)
+    email_message, screen_message = report_data(data=all_crypto)
+
+    # Provide report
+    if options.to_mail:
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.sendmail(sender, rcpt, email_message.as_string())
     else:
-        print(output)
+        print(screen_message)
 
